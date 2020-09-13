4ce1950fa63bcd18cdf5c5389b4bb3f5bd716bc3
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2020-09-13 16:36:45 +0900

    Updates

---
+++ precache-manifest.87e107103048c1495193f01f4866d081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfe0f6babed8d68717019787aa2d067",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "876cc64a8c3c2dda712c",
+    "url": "/movie_app/static/css/main.92b2f872.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876cc64a8c3c2dda712c",
+    "url": "/movie_app/static/js/main.0efdd35d.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
